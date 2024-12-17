1e6355bb5ff09a0c77ae36d772c9770ef594649b
AdwitM <adwitmittal@gmail.com>
2024-12-18 00:46:41 +0530

    feat(cdk-tests): move test files

--- cdk/test/test_create_payment.py
+++ cdk/test/test_create_payment.py
@@ -6,7 +6,7 @@ from unittest.mock import patch
 from moto import mock_aws
 import boto3
 
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../..")))
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../")))
 
 from lambdas.payments.create_payment import handler
 
